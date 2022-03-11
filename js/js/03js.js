c1f0c9c28faee52f48d1b029b5714f5993ff153d
yvandouenel <y.douenel@coopernet.fr>
2022-03-11 15:23:57 +0100

    Premiers objets js.

--- js/js/03js.js
+++ js/js/03js.js
@@ -7,15 +7,26 @@
    * @param {Number} uid 
    * @returns {Object}
    */
-  function User(firstname, name, uid) {
-    // Propriété
+  function User(firstname, lastname, uid) {
+    // Propriétés
     this.firstname = firstname;
-    this.name = name;
+    this.lastname = lastname;
     this.uid = uid;
+
+    // Une propriété de type fonction s'appelle une méthode
+    this.sePresenter = function () {
+      console.log(`Bonjour, je m'appelle ${this.firstname} ${this.lastname}`);
+    }
   }
   // Création d'instance d'utilisateur avec new
-  const bob = new User("bob", "Dylan", 1);
+  const bob = new User("Bob", "Dylan", 1);
+  const simon = new User("Simon", "Molo", 2);
   console.log(`bob : `, bob);
+  // Afficher dans la console le uid
+  console.log(`uid de bob : `, bob.uid);
+
+  // Simon se présente
+  simon.sePresenter();
 
 })();
 
