c9ab10b67f0f0d054637e52231dbebc461eead55
Yvan Douënel <y.douenel@coopernet.fr>
2022-05-09 14:16:51 +0200

    Retour de l'utilisateur coopernet.

--- js/js/10js.js
+++ js/js/10js.js
@@ -7,12 +7,53 @@
       })
       .then((token) => {
         console.log("token : ", token);
+        return getUser(token, "y", "sqdf");
+      })
+      .then(user => {
+        console.log("User : ", user);
       })
       .catch(error => { console.error("Erreur attrapée : ", error) });
 
-      
+    /**
+ * 
+ * @param {string} token 
+ * @param {string} login 
+ * @param {string} pwd 
+ * 
+ * @returns promise
+ */
+    function getUser(token, login, pwd) {
+      // création de la requête
+      console.log("Dans getUser de FetchData");
+      // Va chercher sur le point d'entrée :
+      // https://www.coopernet.fr/user/login?_format=json 
+      return fetch("https://www.coopernet.fr/user/login?_format=json", {// endpoint
+        credentials: "same-origin",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-Token": token
+        },
+        body: JSON.stringify({
+          name: login,
+          pass: pwd
+        })
+      })
+        .then(response => response.json())
+        .then(data => {
+          //console.log("success", data);
+          if (data.current_user === undefined) {
+            throw new Error("Erreur de login");
+          } else {
+            return data;
+          }
+        })
+    };
 
   }
 
 })();
+
+
+
 
